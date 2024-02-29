44c1821187cdaa0c8b6704aeb5dd65488017272a
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2024-02-29 12:30:25 +0000

    Food Placement

---
+++ Assets/Snake2D/Scripts/Food.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public BoxCollider2D gridArea;
+
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    private void RandomizePosition()
+    {
+        Bounds bounds = this.gridArea.bounds;
+
+        float x = UnityEngine.Random.Range(bounds.min.x, bounds.max.x);
+        float y = UnityEngine.Random.Range(bounds.min.y, bounds.max.y);
+
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+            RandomizePosition();
+    }
+}
