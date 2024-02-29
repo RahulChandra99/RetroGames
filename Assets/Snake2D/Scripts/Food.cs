585d398dcd1bf8e53796852c2164729b1259e778
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2024-02-29 12:39:16 +0000

    snake growth system

--- Assets/Snake2D/Scripts/Food.cs
+++ Assets/Snake2D/Scripts/Food.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Food : MonoBehaviour
 {
     public BoxCollider2D gridArea;
-
     private void Start()
     {
         RandomizePosition();
--- Assets/Snake2D/Scripts/Snake.cs
+++ Assets/Snake2D/Scripts/Snake.cs
@@ -6,6 +6,14 @@ public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.zero;
     [SerializeField] private float gameDifficultyValue = 0.02f;
+    private List<Transform> _segments;
+    [SerializeField] private Transform _segmentPrefab;
+
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+    }
 
     private void Update()
     {
@@ -23,6 +31,26 @@ public class Snake : MonoBehaviour
     private void FixedUpdate()
     {
         Time.fixedDeltaTime = gameDifficultyValue;
+
+        for(int i = _segments.Count - 1; i > 0; i--) 
+        {
+            _segments[i].position = _segments[i-1].transform.position;
+        }
+
         this.transform.position = new Vector3(Mathf.Round(this.transform.position.x) + _direction.x, Mathf.Round(this.transform.position.y) + _direction.y, 0.0f);
+    }
+
+    void Grow()
+    {
+        Transform segment = Instantiate(_segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Food"))
+            Grow();
     }
 }
